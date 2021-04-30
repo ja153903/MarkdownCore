b58fed78429ef3b62e9f7a0a58a691b8b5f034ba
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-04-30 12:49:37 -0400

    Added types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import Express from 'express';
+import Express, { Express as IExpress } from 'express';
 
-import { PORT as PORT_TYPE } from '@types';
+import { PORT as PORT_TYPE } from '@types-local';
 
 const PORT: PORT_TYPE = process.env.PORT ?? 8080;
 
-const app = Express();
+const app: IExpress = Express();
 
 app.listen(PORT, () => {
   console.log(`Application running on port ${PORT}`)
