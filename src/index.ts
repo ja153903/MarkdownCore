cc0c467ab42616f201f2fcd7d2f9c2c00bb2e763
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-06-26 11:20:55 -0400

    Setting up prisma

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import Express from 'express';
 
+import { prisma } from './prisma';
+
 const PORT = process.env.PORT ?? 8080;
 
 const app = Express();
 
 app.listen(PORT, () => {
   console.log(`Application running on port ${PORT}`);
+});
+
+process.on('SIGTERM', () => {
+  console.info('SIGTERM signal received. Gracefully shutting down');
+  prisma.$disconnect();
 });
