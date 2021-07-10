8edc5722830dbf3a9ba6aaec593885dc868a8e59
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-07-09 20:17:43 -0400

    Created basic query for creating user

--- src/users/queries.ts
+++ src/users/queries.ts
@@ -1,1 +1,14 @@
-// TODO: Will contain the prisma queries
+import { User } from './interfaces';
+import { prisma } from '../prisma';
+
+async function createUser(user: User) {
+	const { email, name } = user;
+
+	const newUser = await prisma.user.create({
+		data: { email, name }
+	});
+
+	return newUser;
+}
+
+export { createUser };
