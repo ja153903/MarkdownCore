cb6023624b9c84a6764527fd94fcdbb30582f9ea
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-07-10 10:51:16 -0400

    Adding stuff

--- src/users/queries.ts
+++ src/users/queries.ts
@@ -1,9 +1,12 @@
+import logger from '../lib/logger';
 import { User } from './interfaces';
 import { prisma } from '../prisma';
-import logger from '../lib/logger';
 
 async function createUser(user: User) {
 	const { email, name } = user;
+
+	// TODO: MKDN-5/sanitize-and-validate-email
+	// Should have functionality to sanitize and validate the email
 
 	const newUser = await prisma.user.create({
 		data: { email, name }
