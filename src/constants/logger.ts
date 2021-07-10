ed908574ed7c607b1e8f44bef7a314da01c4816f
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-07-10 10:10:27 -0400

    Adding winston integration

---
+++ src/constants/logger.ts
@@ -0,0 +1,15 @@
+export const LOGGING_LEVELS = {
+	ERROR: 'error',
+	WARN: 'warn',
+	INFO: 'info',
+	HTTP: 'http',
+	DEBUG: 'debug'
+};
+
+export const LOGGING_LEVELS_COLORS = {
+	[LOGGING_LEVELS.ERROR]: 'red',
+	[LOGGING_LEVELS.WARN]: 'yellow',
+	[LOGGING_LEVELS.INFO]: 'green',
+	[LOGGING_LEVELS.HTTP]: 'magenta',
+	[LOGGING_LEVELS.DEBUG]: 'white'
+};
